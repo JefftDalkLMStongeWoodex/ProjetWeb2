262cda77f8042844f7b651cc97e9a7eda27a520e
Woodly Boursiquot <woodlyboursiquot@gmail.com>
2022-11-30 14:07:05 -0500

    Creation composante LogoVersion2

--- LaravelApp/VDO/resources/js/Components/ApplicationLogo.vue
+++ LaravelApp/VDO/resources/js/Components/ApplicationLogo.vue
@@ -1,7 +1,7 @@
 <template>
   
     <div class="logo">
-        <img src="../../assets/Logo_PW2_v2.svg" alt="logo VDO">
+        <img src="../../assets/Logo_PW2_v1.svg" alt="logo VDO">
     </div>
 
     
@@ -9,9 +9,11 @@
 
 <style scoped>
 
-.logo--example {
-    background-color:   var( --main-color);
-    padding: 0.50rem;
+.logo {
+  
+    min-height: 5vh;
+    min-width: 14vw;
+    margin-bottom: 8rem;
 }
 
 </style>
