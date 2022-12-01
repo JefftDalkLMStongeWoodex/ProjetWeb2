6df9c7a56f5d28c09e4473756c6a5cf643c2a868
Mathieu St-Onge <stng.mathieu@gmail.com>
2022-12-01 17:32:57 -0500

    Mise à jour AppLayout pour utiliser les routes nommées et début intégration CSS maquette

--- LaravelApp/VDO/resources/js/Layouts/AppLayout.vue
+++ LaravelApp/VDO/resources/js/Layouts/AppLayout.vue
@@ -1,83 +1,72 @@
 <script setup>
 import { ref } from 'vue';
-import SidebarVue from '@/Components/Sidebar.vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import LogoVersion2 from '@/Components/LogoVersion2.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/inertia-vue3';
 
 const showingNavigationDropdown = ref(false);
+
 </script>
 
 <template>
     <div>
         <!-- Page Heading -->
-        <header class="">
-            <nav class="relative flex items-top justify-center h-auto bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-                <NavLink
-                    href="/"
-                    class="mr-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                >
-                    Accueil
-                </NavLink>
-                <Link
-                    :href="route('catalogue')"
-                    class="mr-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                >
-                    Trouver une voiture
-                </Link>
-                <Link
-                    href="/apropos"
-                    class="mr-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                >
-                    À propos
-                </Link>
-                <Link
-                    href="/contact"
-                    class="mr-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                >
-                    Contactez-nous
-                </Link>
-                <template v-if="$page.props.auth.user">
-                    <Link 
-                        :href="route('dashboard')"
-                        class="mr-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                    >
-                        Tableau de bord
-                    </Link>
-                    <Link 
-                        :href="route('logout')"
-                        class="mr-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                    >
-                        Déconnexion
-                    </Link>
-                </template>
-                <template v-else>
-                    <Link 
-                        :href="route('register')"
-                        class="text-sm text-gray-700 dark:text-gray-500 underline"
-                    >
-                        Inscription
-                    </Link>
-                    <Link 
-                        :href="route('login')"
-                        class="text-sm text-gray-700 dark:text-gray-500 underline"
-                    >
-                        Connexion
-                    </Link>
-                </template>
-
+        <header>
+            <nav class="header__navigation">
+                <div class="wrapper">
+                    <div class="header__conteneur">
+                        <a :href="route('accueil')" aria-label="Lien vers page d'accueil">
+                            <LogoVersion2 :href="route('accueil')" class="header__logo"/>
+                        </a>
+                        <div class="header__liens">
+                            <NavLink class="header__lien" :href="route('catalogue')">Trouver votre voiture</NavLink>
+                            <NavLink class="header__lien" href="/contact">Contactez-nous</NavLink>
+                            <template v-if="$page.props.auth.user">
+                                <NavLink class="header__lien" :href="route('dashboard')">Tableau de bord</NavLink>
+                                <NavLink class="header__lien" :href="route('logout')" method="post">Déconnexion</NavLink>
+                            </template>
+                            <template v-else>
+                                <NavLink class="header__lien" :href="route('register')">Inscription</NavLink>
+                                <NavLink class="header__lien" :href="route('login')">Connexion</NavLink>
+                            </template>
+                        </div>
+                    </div>
+                </div>
             </nav>
         </header>
 
         <!-- Page Content -->
         <main>
-            <slot />
+            <div class="wrapper">
+                <slot />
+            </div>
         </main>
     </div>
 
 </template>
 <style scoped>
+.header__navigation {
+    background-color: var(--couleur-secondaire);
+    color: var(--couleur-blanc);
+    text-decoration: none;
+}
 
+.header__conteneur {
+    align-items: center;
+    display: flex;
+    font-family: var(--police-texte);
+    justify-content: flex-start;
+}
+.header__logo {
+    min-width: 170px;
+    width: 10vw;
+}
+.header__liens {
+    margin-left: auto;
+}
 
+.header__lien {
+    font-size: 18px;
+}
 </style>
