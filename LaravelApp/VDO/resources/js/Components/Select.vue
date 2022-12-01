f31097fdffa43e2d3fd6cae6bc88c34ee625d496
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-01 14:27:01 -0500

    Ajout du style au component Select

--- LaravelApp/VDO/resources/js/Components/Select.vue
+++ LaravelApp/VDO/resources/js/Components/Select.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <select>
+    <select class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
         <slot v-for="option in $attrs.options">
         <option  :value="option.id"> {{option.nom}}</option>
         </slot>
