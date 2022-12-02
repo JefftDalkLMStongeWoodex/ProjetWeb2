32f5460de0e71d0fd24d8b5679271192fc0a8900
Woodly Boursiquot <woodlyboursiquot@gmail.com>
2022-12-02 16:13:38 -0500

    Changer couleur du btn principale pour le darkmode

--- LaravelApp/VDO/resources/js/Components/PrimaryButton.vue
+++ LaravelApp/VDO/resources/js/Components/PrimaryButton.vue
@@ -20,7 +20,7 @@ defineProps({
 button{
 width: 100%;
 padding: 1.0rem;
-color: #fff;
+color:var(--couleur-blanc);
 border-radius:  5px;
 background: var(--couleur-principale);
 font-family: var(--police-texte);
@@ -34,7 +34,9 @@ transition: all 0.4s ease 0s
 
 button:hover{
 
-    background:var(--couleur-secondaire);
+    background: var(--couleur-principale);
+    opacity: 0.85;
+    color: var(--couleur-secondaire);
     letter-spacing: 1px;
     -webkit-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
     -moz-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
