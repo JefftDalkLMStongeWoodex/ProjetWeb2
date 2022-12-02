6665414b353d96b7d7019b591fad901d362e182c
Mathieu St-Onge <stng.mathieu@gmail.com>
2022-12-02 17:21:24 -0500

    Ajout des fichiers de langue pour la page voiture du tableau de bord

--- LaravelApp/VDO/lang/fr/voiture.php
+++ LaravelApp/VDO/lang/fr/voiture.php
@@ -1,6 +1,11 @@
 <?php
 return[
     'titre' => 'Liste des voitures',
-    'id' => 'id de la voiture',
+    'head_titre' => 'Tableau de bord | Voitures',
+    'constructeur' => 'Constructeur',
+    'modele' => 'Modèle',
+    'prix_paye' => 'Prix payé',
+    'statut' => 'Statut',
+    'non_disponible' => 'Non disponible'
 ];
 ?>
