fa320263d589f939eb5ba3baffc387731335dce0
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-02 11:47:27 -0500

    Bug fix du component Select qui n'envoie pas la valeur au form

--- LaravelApp/VDO/resources/js/Components/Select.vue
+++ LaravelApp/VDO/resources/js/Components/Select.vue
@@ -1,9 +1,26 @@
 <script>
+export default {
+    props:{
+        options: {
+            type: Array,
+            default: () => []
+        },
+        value:{
+            default: ''
+        }
+    },
+    methods: {
+        handleChange(e) {
+            this.$emit('update:modelValue',e.target.value);
+        }
+    }
+}
 </script>
 
 <template>
-    <select class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
-        <slot v-for="option in $attrs.options" :key="option.id">
+    <select @change="handleChange" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
+        <option  value=""></option>
+        <slot v-for="option in $props.options" :key="option.id">
         <option  :value="option.id"> {{option.nom}}</option>
         </slot>
     </select>
