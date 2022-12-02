cad945e1d983296ad15ce7de08520917cd410a13
Jephte Damas <jephtedamas@hotmail.ca>
2022-12-02 12:26:20 -0500

    Ajout Fr et En dans nav

--- LaravelApp/VDO/resources/js/Layouts/AppLayout.vue
+++ LaravelApp/VDO/resources/js/Layouts/AppLayout.vue
@@ -15,9 +15,19 @@ const showingNavigationDropdown = ref(false);
         <header>
             <nav class="header__navigation">
                 <div class="wrapper">
+                    <div class="header__langSelect">
+                        <NavLink class="header__lang" :href="route('lang', 'en')">
+                            English
+                        </NavLink>
+
+                        <NavLink class="header__lang" :href="route('lang', 'fr')">
+                            Français
+                        </NavLink>
+                    </div>
+                    
                     <div class="header__conteneur">
                         <a :href="route('accueil')" aria-label="Lien vers page d'accueil">
-                            <LogoVersion2 :href="route('accueil')" class="header__logo"/>
+                            <LogoVersion2 :href="route('accueil')" class="header__logo" />
                         </a>
                         <div class="header__liens">
                             <NavLink class="header__lien" :href="route('catalogue')">Trouver votre voiture</NavLink>
@@ -25,7 +35,8 @@ const showingNavigationDropdown = ref(false);
                             <NavLink class="header__lien" href="/contact">Contactez-nous</NavLink>
                             <template v-if="$page.props.auth.user">
                                 <NavLink class="header__lien" :href="route('dashboard')">Tableau de bord</NavLink>
-                                <NavLink class="header__lien" :href="route('logout')" method="post">Déconnexion</NavLink>
+                                <NavLink class="header__lien" :href="route('logout')" method="post">Déconnexion
+                                </NavLink>
                             </template>
                             <template v-else>
                                 <NavLink class="header__lien" :href="route('register')">Inscription</NavLink>
@@ -49,8 +60,14 @@ const showingNavigationDropdown = ref(false);
 <style scoped>
 .header__navigation {
     background-color: var(--couleur-secondaire);
-    color: var(--couleur-blanc);
+    color: black;
     text-decoration: none;
+}
+
+.header__langSelect {
+    display: flex;
+    justify-content: flex-end;
+    padding-top: 10px;
 }
 
 .header__conteneur {
@@ -59,10 +76,12 @@ const showingNavigationDropdown = ref(false);
     font-family: var(--police-texte);
     justify-content: flex-start;
 }
+
 .header__logo {
     min-width: 170px;
     width: 10vw;
 }
+
 .header__liens {
     margin-left: auto;
 }
