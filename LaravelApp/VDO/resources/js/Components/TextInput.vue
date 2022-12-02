22eb2aa08b3252eaa85e180f7241743ba1df22fd
Woodly Boursiquot <woodlyboursiquot@gmail.com>
2022-12-02 16:30:21 -0500

    Changer focus des textes inputs pour le dark mode

--- LaravelApp/VDO/resources/js/Components/TextInput.vue
+++ LaravelApp/VDO/resources/js/Components/TextInput.vue
@@ -18,9 +18,21 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+        class="boite-saisie"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         ref="input"
     />
 </template>
+<style scoped>
+
+.boite-saisie{
+    background-color: var(--couleur-secondaire);
+    color: var(--couleur-blanc);
+    border-color:  var(--couleur-blanc);
+}
+.boite-saisie:focus {
+    border: 0.5px solid var(--couleur-principale);
+  }
+
+</style>
