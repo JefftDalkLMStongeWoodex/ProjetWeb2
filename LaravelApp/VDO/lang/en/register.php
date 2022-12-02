d2a2d3b305229d3dcec513d2aa997d49ed509199
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-02 14:05:36 -0500

    Ajout du dictionnaire pour Anglais de register

---
+++ LaravelApp/VDO/lang/en/register.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'name' => 'Name',
+    'email' => 'Email',
+    'password' => 'Password',
+    'first_name' => 'First name',
+    'anniversaire' => 'Birthday',
+    'adresse' => 'Address',
+    'code_postal' => 'Postal Code',
+    'telephone' => 'Phone Number',
+    'telephone_portable' => 'Cellphone Number',
+    'villes' => 'City'
+];
