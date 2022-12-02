084c50b07be7bede08f0a6439b3efa1376cf5fe5
Woodly Boursiquot <woodlyboursiquot@gmail.com>
2022-12-02 15:59:44 -0500

    Ajout du css en dark mode pour les composants InputLabel

--- LaravelApp/VDO/resources/js/Components/InputLabel.vue
+++ LaravelApp/VDO/resources/js/Components/InputLabel.vue
@@ -3,8 +3,17 @@ defineProps(['value']);
 </script>
 
 <template>
-    <label class="block font-medium text-sm text-gray-700 dark:text-gray-300">
+    <label class="block font-medium text-sm text-gray-700 dark:text-gray-300 etiquette">
         <span v-if="value">{{ value }}</span>
         <span v-else><slot /></span>
     </label>
 </template>
+<style scoped>
+
+.etiquette{
+   font-family: var(--police-titre);
+   color: var(--couleur-principale);
+    
+}
+
+</style>
