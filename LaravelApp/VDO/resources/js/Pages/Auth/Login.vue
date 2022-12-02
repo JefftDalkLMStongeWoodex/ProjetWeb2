b041ac6bbe29cbce1bcbe037e9b755dec10f28d6
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-02 16:14:09 -0500

    Application du multilingue au form de login

--- LaravelApp/VDO/resources/js/Pages/Auth/Login.vue
+++ LaravelApp/VDO/resources/js/Pages/Auth/Login.vue
@@ -28,7 +28,7 @@ const submit = () => {
 
 <template>
     <GuestLayout>
-        <Head title="Log in" />
+        <Head :title="langLogin.login" />
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
             {{ status }}
@@ -36,7 +36,7 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
+                <InputLabel for="email" :value="langLogin.email" />
 
                 <TextInput
                     id="email"
@@ -52,7 +52,7 @@ const submit = () => {
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                <InputLabel for="password" :value="langLogin.password" />
 
                 <TextInput
                     id="password"
@@ -69,7 +69,7 @@ const submit = () => {
             <div class="block mt-4">
                 <label class="flex items-center">
                     <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
+                    <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{langLogin.remember}}</span>
                 </label>
             </div>
 
@@ -79,11 +79,11 @@ const submit = () => {
                     :href="route('password.request')"
                     class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                 >
-                    Forgot your password?
+                    {{langLogin.forgot}}
                 </Link>
 
                 <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
+                    {{langLogin.login}}
                 </PrimaryButton>
             </div>
         </form>
