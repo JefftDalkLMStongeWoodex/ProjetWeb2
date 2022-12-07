5fdb440c3e97cf36e977358a8a522c73ff50d514
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-07 18:02:39 -0500

    Ajout de controlleur pour Images

--- LaravelApp/VDO/app/Http/Controllers/ImageController.php
+++ LaravelApp/VDO/app/Http/Controllers/ImageController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Lang;
 
 class ImageController extends Controller
 {
     //
+    function create(){
+        $langAjoutImage = Lang::get('ajoutimage');
+        return Inertia::render('Dashboard/AjoutImage',[
+            'langAjoutImage' => $langAjoutImage,
+        ]);
+    }
 }
