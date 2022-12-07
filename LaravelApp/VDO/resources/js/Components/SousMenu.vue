f89071e07e69298d6208bdcb61e9b7dfc411de1e
Jephte Damas <jephtedamas@hotmail.ca>
2022-12-07 09:13:46 -0500

    ajout component sousmenu

---
+++ LaravelApp/VDO/resources/js/Components/SousMenu.vue
@@ -0,0 +1,66 @@
+<script>
+  export default {
+    name: 'SousMenu',
+    provide () {
+      return {
+        sharedState: this.sharedState
+      }
+    },
+    data () {
+      return {
+        sharedState:{
+          active: false
+        } 
+      }
+    },
+    methods: {
+      toggle () {
+        this.sharedState.active = !this.sharedState.active
+      }
+    }
+}
+</script>
+
+
+<template>
+  <div @click="toggle">
+    <slot name="toggler">
+
+      <div class="barre-des-filtres__liste">
+        <button class="barre-des-filtres__liste">MARQUE & MODELE</button>
+        <button class="ml-2">PRIX</button> 
+        <button class="ml-2">COULEUR</button>
+        <!-- <button>KM</button>
+        <button>CARROSSERIE</button>
+        <button>TRANSMISSION</button>
+        <button>TRACTION</button>
+        <button>CONDITION</button> 
+        <button><a>CYLINDRE</a></button>
+        <button><a>CARBURANT</a></button> -->
+      </div>
+
+    </slot>
+    <slot/>
+  </div>
+</template>
+
+
+<style>
+.barre-des-filtres__liste {
+  align-items:flex-start;
+  cursor: pointer;
+  color: var(--couleur-blanc);
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  justify-content: center;
+  padding-left: 10px;
+}
+
+
+
+.barre-des-filtres__liste {
+  min-width: 78px;
+}
+
+</style>
