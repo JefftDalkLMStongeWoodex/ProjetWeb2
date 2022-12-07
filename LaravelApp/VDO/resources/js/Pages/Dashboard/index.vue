44eae79174ad72b9e65fff504651fa0045b826de
Woodly Boursiquot <woodlyboursiquot@gmail.com>
2022-12-07 10:44:36 -0500

    Creation du fichier index pour la section tableau de bord

---
+++ LaravelApp/VDO/resources/js/Pages/Dashboard/index.vue
@@ -0,0 +1,49 @@
+<script setup>
+import TableauDeBordLayout from '@/Layouts/TableauDeBord.vue';
+import SidebarVue from '@/Components/Sidebar.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    langDashboard: Object,
+})
+</script>
+
+<template>
+    <Head title="Dashboard" />
+    <TableauDeBordLayout> 
+    <div class="flex"> 
+        <SidebarVue :lang="langDashboard"/>
+
+      
+            
+            <div class="application">      
+                <div class="contenu--tableau-de-bord ">
+
+                    <h2 class="titre--page">{{langDashboard.titre}}</h2>
+                    <div class="py-12">
+                        <div class="">
+                            <div class="">
+                                <div class="">{{langDashboard.texte}}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+     
+    </div> 
+</TableauDeBordLayout>
+</template>
+<style scoped>
+.contenu--tableau-de-bord {
+    flex: 1 1 0;
+    margin: 3em;
+    line-height: 1.5;
+    font-weight: 500;
+    color: var(--couleur-secondaire);
+}
+
+.titre--page{
+    font-weight: bold;
+    font-size: x-large;
+}
+</style>
