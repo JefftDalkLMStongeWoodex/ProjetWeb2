a5bd6d13a8f8e7f092fa9db7a2d396b0f7900aab
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-07 09:59:49 -0500

    Modification pour les props envoyer au Catalogue

--- LaravelApp/VDO/app/Http/Controllers/CatalogueController.php
+++ LaravelApp/VDO/app/Http/Controllers/CatalogueController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 use App\Models\Voiture;
 use App\Models\Modele;
 use App\Models\Constructeur;
+use App\Models\Transmission;
+use App\Models\Corps;
+use App\Models\GroupeMotopropulseur;
+use App\Models\Carburant;
+use App\Models\Etat;
+use Inertia\Inertia;
 
 use Lang;
 
@@ -13,16 +19,19 @@ class CatalogueController extends Controller
 {
     //
     function index(){
-        $voiture = Voiture::where('statut_voitures_id', 1)->limit(25);
-        $modele = Modele::all();
-        $constructeur = Constructeur::all();
-        $langCatalogue = Lang::get('catalogue');
-        
-        return Inertia::render('Dashboard/Voiture', [
-            'langCatalogue' => $langCatalogue,
-            'voitures' => $voiture,
-            'modele' => $modele,
-            'constructeur' => $constructeur,
+        $voitures = Voiture::where('statut_voitures_id', 1)->limit(25);
+
+        return Inertia::render('Catalogue', [
+            'langAppLayout' => Lang::get('app_layout'),
+            'langCatalogue' => Lang::get('catalogue'),
+            'voitures' => $voitures,
+            'modele' => Modele::all(),
+            'constructeur' => Constructeur::all(),
+            'corps' => Corps::all(),
+            'transmissions' => Transmission::all(),
+            'groupeMotopropulseurs' => GroupeMotopropulseur::all(),
+            'carburants' => Carburant::all(),
+            'etats' => Etat::all(),
         ]);
     }
 }
