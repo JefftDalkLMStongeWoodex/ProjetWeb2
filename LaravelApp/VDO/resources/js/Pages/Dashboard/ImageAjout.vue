e656883a0cad87e4352548eac4feaaa8f6243b96
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-08 15:10:12 -0500

    Ajout fichier pour image

--- LaravelApp/VDO/resources/js/Pages/Dashboard/ImageAjout.vue
+++ LaravelApp/VDO/resources/js/Pages/Dashboard/ImageAjout.vue
@@ -1,7 +1,16 @@
 <script>
+const { default: InputLabel } = require('vendor/laravel/breeze/stubs/inertia-react/resources/js/Components/InputLabel');
+const { default: TextInput } = require('vendor/laravel/breeze/stubs/inertia-react/resources/js/Components/TextInput');
+
 </script>
 <template>
-<h1>Balls</h1>
+<form action="">
+    @csrf
+    <InputLabel></InputLabel>
+    <input type="file" name="" id="">
+    <InputLabel></InputLabel>
+    <TextInput></TextInput>
+</form>
 </template>
 <style scoped>
 </style>
