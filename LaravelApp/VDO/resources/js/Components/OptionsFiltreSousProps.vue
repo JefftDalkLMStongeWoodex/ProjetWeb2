a198329d7b48cd451ed75623a6336ea80b84bf69
Jephte Damas <jephtedamas@hotmail.ca>
2022-12-08 18:00:10 -0500

    changement de nom sousMenuItem a optionsFiltreSousProps

---
+++ LaravelApp/VDO/resources/js/Components/OptionsFiltreSousProps.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "OptionsFiltreSousProps"
+};
+</script>
+
+<template>
+  <a href="#" class="block hover:bg-gray-200 my-1 px-4 py-1 font-medium text-gray-800">
+    <slot/>
+  </a>
+</template>
+
+<style>
+  
+</style>
+
