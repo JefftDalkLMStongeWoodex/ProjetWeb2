42c94072023d3c7cb7ca9956f97c5cae075758ae
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-09 16:03:35 -0500

    Creation du composant de filtre pour le catalogue

---
+++ LaravelApp/VDO/resources/js/Components/SectionFiltres.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props:{
+        titre: {
+            type: String,
+            default: ''
+        },
+        options: {
+            type: Array,
+            default: () => []
+        },
+        colonneAffichee: String,
+    },
+    methods: {
+        handleChange(e) {
+            this.$emit('update:modelValue', e.target.value);
+        }
+    },
+}
+</script>
+<template>
+    <details>
+        <summary>{{titre}}</summary>
+        <ul>
+            <li v-for="option in options">
+            <input type="checkbox" :name="option" id="option">
+            <strong>{{$page.props.lang.locale == "en" ? option[colonneAffichee + "_en"] : option[colonneAffichee]}}</strong>
+            </li>
+        </ul>
+    </details>
+</template>
+<style scoped>
+</style>
