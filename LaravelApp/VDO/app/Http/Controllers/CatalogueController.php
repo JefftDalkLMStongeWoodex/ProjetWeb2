5958d2c79459343de12817cc3068515465b538ee
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-09 12:22:53 -0500

    Correction des requetes SQL pour le catalogue

--- LaravelApp/VDO/app/Http/Controllers/CatalogueController.php
+++ LaravelApp/VDO/app/Http/Controllers/CatalogueController.php
@@ -19,9 +19,14 @@ class CatalogueController extends Controller
 {
     //
     function index(){
-        $voitures = Voiture::where('statut_voitures_id', '1')->get(['id', 'annee', 'prix_paye', 'kilometrage', 'corps_id','modeles_id', 'transmissions_id', 'groupe_motopropulseurs_id', 'carburants_id', 'commandes_id', 'etats_id']);
+        // $voitures = Voiture::where('statut_voitures_id', '1')->get(['id', 'annee', 'prix_paye', 'kilometrage', 'corps_id','modeles_id', 'transmissions_id', 'groupe_motopropulseurs_id', 'carburants_id', 'etats_id']);
+        $voitures = Voiture::where('statut_voitures_id', '1')->join('modeles', function($join) {
+            $join->on('voitures.modeles_id','=','modeles.id')->join('constructeurs', function($join){
+                $join->on('modeles.constructeurs_id','=','constructeurs.id');
+            });
+        })->get(['voitures.id','voitures.prix_paye AS prix','voitures.kilometrage','voitures.annee','modeles.nom AS modele','constructeurs.nom as constructeurs','voitures.corps_id']);
         foreach($voitures as $voiture) {
-            $voiture['prix_paye'] *= 1.25;
+            $voiture['prix'] *= 1.25;
         }
         return Inertia::render('Catalogue', [
             'langAppLayout' => Lang::get('app_layout'),
