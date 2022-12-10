d777e8a3d47d65ef7c496bc4a4f0d5b34d455e0d
Mathieu St-Onge <stng.mathieu@gmail.com>
2022-12-10 18:23:03 -0500

    Création fichiers multilingue pour pages du ImageController

---
+++ LaravelApp/VDO/lang/fr/image.php
@@ -0,0 +1,17 @@
+<?php
+return[
+    'titre' => 'Liste des images',
+    'ajout' => 'Ajouter une image',
+    'nom' => 'Nom (français)',
+    'nom_en' => 'Nom (anglais)',
+    'chemin' => 'Chemin',
+    'alt' => 'Texte alternatif (français)',
+    'alt_en' => 'Texte alternatif (anglais)',
+    'principale' => 'Image principale',
+    'idVoiture' => 'ID voiture',
+    'image' => 'Image',
+    'principale_message' => 'Attention: une seule photo par voiture peut être désignée comme principale.',
+    'principale_erreur' => 'Une image principale existe déjà pour cette voiture.',
+    'soumettre' => 'Ajouter l\'image',
+];
+?>
