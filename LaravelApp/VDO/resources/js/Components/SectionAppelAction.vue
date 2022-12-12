e2172c132fff76d645309f2216ea2b1fa0b9326b
Mathieu St-Onge <stng.mathieu@gmail.com>
2022-12-12 18:23:16 -0500

    Modification composant SectionAppelAction pour affichage des ctas sur la fiche de produit

--- LaravelApp/VDO/resources/js/Components/SectionAppelAction.vue
+++ LaravelApp/VDO/resources/js/Components/SectionAppelAction.vue
@@ -1,15 +1,58 @@
 <script setup>
- 
+  import PrimaryButton from '@/Components/PrimaryButton.vue';
+
 </script>
 
 <template>
-  <h1> Appel a action 
-</h1>
+
+  <div class="AppelAction">
+
+    <div class="titre">
+      <h3> Vous avez des questions ? </h3> </div> 
+    <div class="Bouton_fiche"> 
+      <PrimaryButton> Telephoner </PrimaryButton>
+    <PrimaryButton> Discuter </PrimaryButton>
+
+    </div>
+
+  </div>
+ 
 </template>
 
 <style scoped>
 
+.AppelAction{
+ display: flex;
+ flex-direction: column;
+  margin: auto 0 ;
+  align-items: center;
+  background-color: var(--couleur-secondaire);
+  color: var(--couleur-blanc);
+  padding: 5rem;
 
+}
+
+.titre{
+  padding: 2rem;
+  text-align: center;
+  width: 100vw;
+
+}
+
+.Bouton_fiche {
+  display: flex;
+  flex-direction: row; 
+
+  
+
+
+}
+
+.icon-send {
+  margin-right: 1rem;
+  
+
+}
 
 
 
