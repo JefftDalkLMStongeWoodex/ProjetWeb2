cb105f4eaa0099e3047e26a4fd32d83c9dfb653c
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-15 18:29:45 -0500

    Ajout du composant liste du panier avec placeholder emit de suppression

--- LaravelApp/VDO/resources/js/Components/Checkout/ListePanier.vue
+++ LaravelApp/VDO/resources/js/Components/Checkout/ListePanier.vue
@@ -1,7 +1,38 @@
 <script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+const emit = defineEmits(['suppressionItem'])
+</script>
+<script>
+export default{
+    methods:{
+        suppressionItem(id) {
+            this.$emit('suppressionItem', id);
+        }
+    }
+}
 </script>
 <template>
-    
+    <ul>
+        <li v-for="item of $attrs.liste">
+            <div>
+                <img v-if="item.imagePrincipale[0] != null" :src="`/storage/${item.imagePrincipale[0].chemin}`" class="vehicule-img" />
+                <img v-else src="../../../assets/kiaForte2015.jpg" class="vehicule-img" />
+            </div>
+            <div>
+                <div>
+                    <Link :href="route('voiture.fiche',item.id)">
+                        <p>{{item.annee}} {{item.constructeurs}} {{item.modeles}}</p>
+                    </Link>
+                    <p>
+                        ${{item.prix}}
+                    </p>
+                </div>
+                <button @click="suppressionItem(item.id)">
+                    <i class="fa-solid fa-trash-can"></i>
+                </button>
+            </div>
+        </li>
+    </ul>
 </template>
 <style scoped>
 </style>
--- LaravelApp/VDO/resources/js/Pages/Checkout.vue
+++ LaravelApp/VDO/resources/js/Pages/Checkout.vue
@@ -1,16 +1,26 @@
 <script setup>
 import { Head } from '@inertiajs/inertia-vue3'
 import AppLayout from '@/Layouts/AppLayout.vue'
+import ListePanier from '@/Components/Checkout/ListePanier.vue'
+import InfoCommande from '@/Components/Checkout/InfoCommande.vue'
 const props = defineProps({
     panier: Object,
     langAppLayout: Object,
     langCheckout: Object,
 })
+let commande = props.panier;
 </script>
 <template>
     <AppLayout :lang="langAppLayout">
         <Head title="Checkout" />
-        <h1>{{langCheckout.test}}</h1>
+        <section class="">
+            <ListePanier
+            :liste = "commande"
+            />
+            <InfoCommande
+            :liste = "commande"
+            />
+        </section>
     </AppLayout>
 </template>
 <style scoped>
