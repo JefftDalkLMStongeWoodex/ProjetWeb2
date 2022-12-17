a997196fb7286b5ef84a5018c39df66ec11c14a9
DalkL <dalkiriadis.lukas@gmail.com>
2022-12-17 17:43:38 -0500

    Ajout du total probleme avec le input radio pour le checkout

--- LaravelApp/VDO/resources/js/Components/Checkout/InfoCommande.vue
+++ LaravelApp/VDO/resources/js/Components/Checkout/InfoCommande.vue
@@ -7,10 +7,25 @@ const props = defineProps({
     liste: Object
 })
 const form = useForm({
-  provinces: ''
+  provinces: '',
+  modeExpedition: ''
 })
 </script>
 <script>
+export default{
+    computed:{
+        getTotal() {
+            let total = 0;
+            for(let item of this.$props.liste) {
+                total += item.prix;
+            }
+            return total
+        },
+        // getTaxe() {
+        //     return this.$setup.form
+        // }
+    }
+}
 </script>
 <template>
     <aside>
@@ -28,19 +43,19 @@ const form = useForm({
             <div>
                 <strong>Comment souhaité vous recevoir la commande?</strong>
                 <br>
-                <input type="radio" name="livraison" id="">
+                <input type="radio" name="modeExpedition" v-model="modeExpedition" id="livraison" value="1">
                 <label for="livraison">
                     Expédition au domicile
                 </label>
                 <br>
-                <input type="radio" name="ramassage" id="">
+                <input type="radio" name="modeExpedition" v-model="modeExpedition" id="ramassage" value="2">
                 <label for="ramassage">
-                    Ramassage aux concessionnaire 
+                    Ramassage aux concessionnaire
                 </label>
             </div>
         </form>
         <section>
-            
+            {{form.modeExpedition}}
         </section>
     </aside>
 </template>
