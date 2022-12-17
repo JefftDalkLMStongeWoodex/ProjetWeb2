1a68892e1a7bc9be4277ca2befd28427f9c4f10c
Mathieu St-Onge <stng.mathieu@gmail.com>
2022-12-17 18:57:49 -0500

    Ajout des fichiers de langue pour la page Utilisateurs du Dashboard

---
+++ LaravelApp/VDO/lang/en/utilisateur.php
@@ -0,0 +1,16 @@
+<?php
+return[
+    'nom' => 'Last name',
+    'prenom' => 'First name',
+    'email' => 'Email',
+    'anniversaire' => 'Birthdate',
+    'adresse' => 'Adress',
+    'code_postal' => 'Postal code',
+    'telephone' => 'Phone',
+    'telephone_portable' => 'Cellphone',
+    'privilege' => 'Privilege',
+    'ville' => 'City',
+    'titre' => 'User list'
+
+];
+?>
