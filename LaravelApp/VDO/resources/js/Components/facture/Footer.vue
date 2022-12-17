45446ba886fee883ac5f3acfdc5df038263758cc
Jephte Damas <jephtedamas@hotmail.ca>
2022-12-17 17:55:00 -0500

    modification page facturation

---
+++ LaravelApp/VDO/resources/js/Components/facture/Footer.vue
@@ -0,0 +1,37 @@
+<script setup>
+// import Logo_PW2_v1 from 'assets/Logo_PW2_v1';
+
+</script>
+
+<template>
+  <section class="wrapper">
+    <div class="piedDePage">
+      <ul class="piedDePage__droite">
+        <li><strong>Avant taxes</strong>13000$</li>
+        <li>TPS-5%</li>
+        <li>TVQ-9.975%</li>
+        <li>prix total</li>
+      </ul>
+    </div>
+
+    <div class="piedDePage__gauche">
+      <h5>Notes</h5>
+      <p class="">Un dépot de 40% est requis pour l'avancement du projet.</p>
+    </div>
+  </section>
+
+</template>
+
+<style scoped>
+.piedDePage {
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 1rem;
+}
+.piedDePage__droite {
+  padding-right: 1rem;
+}
+
+</style>
---
+++ LaravelApp/VDO/resources/js/Pages/Facturation.vue
@@ -0,0 +1,38 @@
+<script setup>
+import TableauDeBordLayout from '@/Layouts/TableauDeBord.vue';
+import SidebarVue from '@/Components/Sidebar.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+import Header from '@/Components/facture/Header.vue';
+import Main from '@/Components/facture/Main.vue';
+import Footer from '@/Components/facture/Footer.vue';
+
+defineProps({
+  langDashboard: Object,
+})
+</script>
+
+<template>
+    <Head title="Facture" />
+    <TableauDeBordLayout> 
+        <template #header>
+            <SidebarVue :lang="langDashboard" />         
+        </template>
+      
+        <div class="">
+            <div class="entete">
+                <Header :lang="langDashboard" />
+            </div>
+
+            <div class="corps">
+                <Main :lang="langDashboard" />
+            </div>
+
+            <div class="piedDePage">
+                <Footer :lang="langDashboard" />
+            </div> 
+        </div>
+    </TableauDeBordLayout>
+</template>
+<style scoped>
+
+</style>
