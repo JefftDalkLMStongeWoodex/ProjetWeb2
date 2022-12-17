a03f040d5c7938a55a6ea0d9f4d20042edca8d6f
Jephte Damas <jephtedamas@hotmail.ca>
2022-12-16 23:56:06 -0500

    Ajout traduction Invoices à Factures

--- LaravelApp/VDO/lang/fr/dashboard.php
+++ LaravelApp/VDO/lang/fr/dashboard.php
@@ -3,6 +3,8 @@ return[
     'voiture' => 'Voitures',
     'logout' => 'Déconnexion',
     'titre' => 'Tableau de bord',
-    'texte' => 'Vous êtes connectés!'
+    'texte' => 'Vous êtes connectés!',
+    'lien_facture' => 'Voir la facture',
+    'factures' => 'Factures',
 ];
 ?>
