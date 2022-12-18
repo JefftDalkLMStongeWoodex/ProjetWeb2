2a3a6a65d7bf68f84d16495225eb8d0b73a02b68
Jephte Damas <jephtedamas@hotmail.ca>
2022-12-18 16:55:44 -0500

    ajout page contact

--- LaravelApp/VDO/resources/js/Pages/Contact.vue
+++ LaravelApp/VDO/resources/js/Pages/Contact.vue
@@ -1,117 +1,194 @@
 <script setup>
 import { Head } from '@inertiajs/inertia-vue3';
 import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps({
+  canLogin: Boolean,
+  canRegister: Boolean,
+  langAppLayout: Object,
+});
 </script>
 
 <template>
-
-  <Head title="Contactez-nous" />
-
-  <AppLayout>
-    <div class="mt-medium">
-
-      <div class="grid grid-cols-2 gap-8">
-          <div class="">
-            <h1 class="title">VDO Inc.</h1>
-            <p class="text">3800 R. Sherbrooke E, Montréal, QC H1X 2A2</p>
-            <p class="text">514-254-7131</p>
-            <p class="text">Si vous avez des questions concernant nos véhicules, y compris des sélections de voitures
-              d'occasion,
-              n'hésitez pas à nous les poser. Chez VDO Inc, il n'y a pas de questions idiotes. Nous sommes là pour
-              répondre
-              à toutes vos questions et résoudre tous les problèmes que vous pourriez avoir.
-            </p>
-            <p class=" text mt-2">
-              Utilisez notre formulaire de contact pour exposer votre question ou votre demande. Nous examinerons et
-              répondrons dès que possible en utilisant votre méthode de communication préférée.
-            </p>
-          </div>
-
-          <div>
-            <h1 class="title">Heures d'ouverture</h1>
-            <div class="">
-              <table class="border-separate border-spacing-5 border border-slate-500">
-                <tbody>
-                  <tr class="text-center">
-                    <th class="border border-slate-700">Lundi au Vendredi</th>
-                    <td class="border border-slate-700">10:00 - 18:00</td>
-                  </tr>
-                  <tr class="text-center">
-                    <th class="border border-slate-700">Samedi</th>
-                    <td class="border border-slate-700">10:00 - 16:00</td>
-                  </tr>
-                  <tr class="text-center">
-                    <th class="border border-slate-700">Dimanche</th>
-                    <td class="border border-slate-700">Fermé</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-
-          <div class="mt-medium">
-            <h2 class=""> Pour toute question </h2>
-            <form action="#" class="space-y-4">
-              <div>
-                <label for="email" class="block mb-2">Courriel</label>
-
-                <input type="email" id="email"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                  required>
-              </div>
-              <div>
-                <label for="subject" class="block mb-2">Sujet</label>
-                <input type="text" id="subject"
-                  class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                  placeholder="Let us know how we can help you" required>
-              </div>
-              <div class="sm:col-span-2">
-                <label for="message" class="block mb-2 ">Votre message</label>
-
-                <textarea id="message" rows="4"
-                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                </textarea>
-              </div>
-              <button>Submit</button>
-            </form>
-          </div>
-
-          <div>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque excepturi iure ipsum ab! Libero
-              reiciendis
-              dolorum animi quasi inventore delectus distinctio, amet maxime, a rem quam cupiditate dicta cum nobis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque excepturi iure ipsum ab! Libero
-              reiciendis
-              dolorum animi quasi inventore delectus distinctio, amet maxime, a rem quam cupiditate dicta cum nobis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque excepturi iure ipsum ab! Libero
-              reiciendis
-              dolorum animi quasi inventore delectus distinctio, amet maxime, a rem quam cupiditate dicta cum nobis!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque excepturi iure ipsum ab! Libero
-              reiciendis
-              dolorum animi quasi inventore delectus distinctio, amet maxime, a rem quam cupiditate dicta cum nobis!</p>
-          </div>
+  <AppLayout :lang="langAppLayout">
+    <Head title="Contact" />
+    <section class="">
+      <div class="box">
+        <div class="text">
+        <h1>Contact <span class="red">Us</span></h1>
+        <hr class="redline">
+        <p>Have Questions ? We have answers ( may be )</p>
         </div>
       </div>
-</AppLayout>
 
+      <div class="wrapper">
+        <div class="touch">
+        <h2>Get in touch</h2>
+        <hr class="redline">
+      </div>
+  
+      <form action="" method="POST" name="contact-form">
+      <div class="container form-margin">
+        <div class="row">
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>     <div class="col-lg-4 col-md-4 col-sm-4">
+          <div class="form-group">
+        <input type="text" class="form-control" placeholder="First Name" ng-model="firstname" name="firstname" required>    
+          </div>
+        <div class="form-group">
+        <input type="text" class="form-control" placeholder="Company Name" ng-model="company" name="company" required>    
+          </div>
+        <div class="form-group">
+        <input type="text" class="form-control" placeholder="Phone" ng-model="phone" name="firstname" required>    
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4">
+          <div class="form-group">
+        <input type="text" class="form-control" placeholder="Last Name" ng-model="lastname" name="lastname" required>    
+          </div>
+          <div class="form-group">
+        <input type="email" class="form-control" placeholder="E-mail" ng-model="email" name="email" required>    
+          </div>
+          <div class="form-group">
+        <input type="text" class="form-control" placeholder="Course" ng-model="course" name="course" required>    
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
+        </div>
+      </div>
+      
+      <div class="container">
+        <div class="row">
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
+        <div class="col-lg-8 col-md-8 col-sm-8">
+          <div class="form-group">
+        <textarea class="form-control" rows="6" placeholder="Message" ng-model="message" name="message" required></textarea>
+      </div>  
+          <div class="pager">
+          <button type="submit" class="btn btn-success">SEND MESSAGE</button>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"></div>
+      
+      </div>
+      </form>
+    <!--address-->
+
+  <div class="container address">
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+    <div class="col-lg-4 col-md-4 col-sm-4">
+      <h3>Our Address</h3>
+      <div class="redline-address"></div>
+      <p>3800 Sherbrooke St E</p>
+      <p>Montreal,</p>
+      <p>Quebec</p>
+      <p>H1X 2A2</p>
+      <div class="phone-e">
+      <p><span class="glyphicon glyphicon-envelope"> </span> info@vdoInc  /.com</p>
+      <p><span class="glyphicon glyphicon-phone"></span> +91-97427 66186</p>
+      </div>
+    </div>
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+      <!--map-->
+      <div style="width: 100%"><iframe scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Coll%C3%A8ge%20de%20Maisonneuve,%203800%20Sherbrooke%20St%20E,%20Montreal,%20Quebec%20H1X%202A2+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%" height="600" frameborder="0"><a href="https://www.maps.ie/distance-area-calculator.html">distance maps</a></iframe></div>
+
+    </div>
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+
+  </div>
+  
+  <div class="bottom-gap"></div>
+      </div>
+      
+      
+    </section>
+  </AppLayout>
 </template>
-
-<style>
-table {
-  width: 100%;
+<style scoped>
+.box{
+  background: url("../../assets/img8.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding-bottom: 500px;
+  color: white;
+  text-align:center;
+  z-index:1;
 }
-
-.mt-medium {
-  margin-top: 5rem;
+.text{
+    padding: 100px 0;
 }
-
-.title {
-  color: var(--couleur-secondaire);
-  font-family: var(--police-titre);
-  font-size: 1.5rem;
+.box p{
+  font-size:18px;
 }
-
-.text {
-  color: var(--couleur-secondaire);
+.red{
+  color:var(--couleur-principale);
+}
+.redline{
+  width:100px;
+  height:3px;
+  background-color:var(--couleur-principale);
+  border:none;
+}
+.touch h2{
+  padding-top: 20px;
+  text-align: center;
+}
+.form-margin{
+  margin-top: 40px;
+}
+.left{
+  text-align:left;
+}
+h3{
+  font-variant:bold;
+}
+.redline-address{
+  border:none;
+  height:3px;
+  background-color:var(--couleur-principale);
+  width:140px;
+  margin-bottom:20px;
+}
+.address{
+  padding-top: 50px;
+}
+.address p{
+  font-weight:bold;
+  color: #676565;
+  margin:3px;
+}
+.phone-e{
+  padding:15px 0;
+}
+.logo img{
+  height:50px;
+  width:150px;
+  z-index:2;
+  float:left;
+  margin-top:10px;
+}
+.bottom-gap{
+  margin-bottom:100px;
+}
+/*validation css*/
+input.ng-valid {
+  background-color:#dff0d8;
+}
+input.ng-invalid {
+    background-color:#f2dede;
+}
+input.ng-pristine{
+  background-color:white;
+}
+textarea.ng-invalid {
+    background-color:#f2dede;
+}
+textarea.ng-pristine{
+  background-color:white;
+}
+textarea.ng-valid {
+  background-color:#dff0d8;
 }
 </style>
+   
