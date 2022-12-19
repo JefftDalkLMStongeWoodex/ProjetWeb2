6276fc6555232d5dd6dc1a517354e753c7851cab
Woodly Boursiquot <woodlyboursiquot@gmail.com>
2022-12-19 10:42:49 -0500

    Ajout de la composante Pagination pour le catalogue

---
+++ LaravelApp/VDO/resources/js/Components/Pagination.vue
@@ -0,0 +1,45 @@
+<script>
+import { Link } from '@inertiajs/inertia-vue3'
+export default {
+  components: {
+    Link,
+  },
+  props: {
+    paginer: Object, 
+  },
+}
+
+
+
+</script>
+<template>
+  
+    <div class="pagination">  
+    <div v-if="paginer.data.length > 0">
+        <div class="">
+
+            <Link
+           
+            v-for="pagination in paginer.links"
+            :href="pagination.url"
+            v-html="pagination.label"
+            
+            /> 
+         
+
+        </div>
+    </div>
+</div>
+</template>
+<style scoped>
+.pagination{
+
+margin: 2rem ;
+padding: 2rem;
+}
+.pagination a{
+padding: 0rem 0.4rem;
+    }
+
+</style>
+
