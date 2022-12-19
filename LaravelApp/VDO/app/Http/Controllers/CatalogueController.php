c74517b697ea96890fed8067f029f18a23720ad4
Mathieu St-Onge <stng.mathieu@gmail.com>
2022-12-19 18:55:20 -0500

    Modif CatalogueController pour corriger la requête de la fonction index et pour créer la méthode de recherche

--- LaravelApp/VDO/app/Http/Controllers/CatalogueController.php
+++ LaravelApp/VDO/app/Http/Controllers/CatalogueController.php
@@ -17,11 +17,7 @@ use Lang;
 
 class CatalogueController extends Controller
 {
-    //
-
-    //Dispatch::where('user_id', Auth::id())->paginate(10);
-
-    function index(){
+    public function index(){
         $voitures = Voiture::where('statut_voitures_id', '1')
         ->join('modeles', function($join) {
             $join->on('voitures.modeles_id','=','modeles.id')->join('constructeurs', function($join){
@@ -43,9 +39,6 @@ class CatalogueController extends Controller
         ->join('etats', function($join) {
             $join->on('voitures.etats_id','=','etats.id');
         })
-        ->paginate(3, array('voitures.id', 'voitures.prix_paye AS prix', 'voitures.kilometrage','voitures.annee', 'modeles.nom AS modeles','constructeurs.nom AS constructeurs','carburants.type AS carburants','groupe_motopropulseurs.type AS groupeMotopropulseurs', 'transmissions.type AS transmissions','corps.type AS corps','etats.nom AS etats'));
-        
-    /*
         ->get([
             'voitures.id',
             'voitures.prix_paye AS prix',
@@ -58,9 +51,7 @@ class CatalogueController extends Controller
             'transmissions.type AS transmissions',
             'corps.type AS corps',
             'etats.nom AS etats',
-        ])
-*/
-        
+        ]);
 
         // On ajout un taux de 25% pour la  marge de profit
         foreach($voitures as $voiture) {
@@ -85,5 +76,74 @@ class CatalogueController extends Controller
             'carburants' => Carburant::all(),
             'etats' => Etat::all(),
         ]);
+    }
+
+    public function recherche($recherche) {
+        $texteRecherche = "%$recherche%";
+
+        $voitures = Voiture::where('statut_voitures_id', '1')
+        ->where(function ($query) use ($texteRecherche) {
+            $query->where('modeles.nom', 'LIKE', $texteRecherche)
+                ->orWhere('constructeurs.nom', 'LIKE', $texteRecherche)
+                ->orWhere('voitures.annee', 'LIKE', $texteRecherche);
+        })
+        ->join('modeles', function($join) {
+            $join->on('voitures.modeles_id','=','modeles.id')->join('constructeurs', function($join){
+                $join->on('modeles.constructeurs_id','=','constructeurs.id');
+            });
+        })
+        ->join('groupe_motopropulseurs', function($join) {
+            $join->on('voitures.groupe_motopropulseurs_id','=','groupe_motopropulseurs.id');
+        })
+        ->join('transmissions', function($join) {
+            $join->on('voitures.transmissions_id','=','transmissions.id');
+        })
+        ->join('carburants', function($join) {
+            $join->on('voitures.carburants_id','=','carburants.id');
+        })
+        ->join('corps', function($join) {
+            $join->on('voitures.corps_id','=','corps.id');
+        })
+        ->join('etats', function($join) {
+            $join->on('voitures.etats_id','=','etats.id');
+        })
+        ->get([
+            'voitures.id',
+            'voitures.prix_paye AS prix',
+            'voitures.kilometrage',
+            'voitures.annee',
+            'modeles.nom AS modeles',
+            'constructeurs.nom AS constructeurs',
+            'carburants.type AS carburants',
+            'groupe_motopropulseurs.type AS groupeMotopropulseurs',
+            'transmissions.type AS transmissions',
+            'corps.type AS corps',
+            'etats.nom AS etats',
+        ]);
+        
+        // On ajout un taux de 25% pour la  marge de profit
+        foreach($voitures as $voiture) {
+            $voiture['prix'] *= 1.25;
+            $voiture['imagePrincipale'] = $voiture->imagePrincipale($voiture['id']);
+        }
+        
+        $paginer = $voitures;
+        
+        return Inertia::render('Catalogue', [
+            'langAppLayout' => Lang::get('app_layout'),
+            'langCatalogue' => Lang::get('catalogue'),
+            'voitures' => $voitures,
+            'paginer' => $paginer,
+
+            
+            'modeles' => Modele::all(),
+            'constructeurs' => Constructeur::all(),
+            'corps' => Corps::all(),
+            'transmissions' => Transmission::all(),
+            'groupeMotopropulseurs' => GroupeMotopropulseur::all(),
+            'carburants' => Carburant::all(),
+            'etats' => Etat::all(),
+        ]);
+        
     }
 }
