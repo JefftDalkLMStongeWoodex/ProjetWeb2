a263fa7bffd814dd8305d8799eb0095aa1e291df
Mathieu St-Onge <stng.mathieu@gmail.com>
2022-12-19 17:44:22 -0500

    Template blade pour générer pdf de la facture à envoyer par courriel

--- LaravelApp/VDO/resources/views/facture.blade.php
+++ LaravelApp/VDO/resources/views/facture.blade.php
@@ -3,8 +3,51 @@
 <head>
 <meta charset="utf-8">
 <title>PDF</title>
+<style>
+    ul {
+        list-style-type: none;
+    }
+</style>
 </head>
 <body>
-{{$voiture}}
+    <header>
+        <h1>Facture pour achat de véhicule(s)</h1>
+        <ul>
+            <li>VDO INC</li>
+            <li>3800 R. Sherbrooke E</li>
+            <li>Montréal, QC</li>
+            <li>H1X 2A2</li>
+            <li>(514) 254-7131</li>
+        </ul>
+    </header>
+    <hr>
+    <main>
+        <div>
+            <h2>Facturé à</h2>
+            <ul>
+                <li>{{ $user_nom }}</li>
+                <li>{{ $user_adresse }}</li>
+                <li>{{ $user_code_postal }}</li>
+                <li>{{ $user_telephone }}</li>
+                <li>{{ $user_email }}</li>
+            </ul>
+        </div>
+        <div>
+            <h2>Détail de la commande</h2>
+            {!! $domVoitures !!}
+        </div>
+        <div>
+            <ul>
+                <li>Sous-total: {{ $montantTotal }}
+                {!! $domTaxes !!}
+                <li>Total: {{ $montantApresTaxe }}</li>
+            </ul>
+        </div>
+    </main>
+    <hr>
+    <section>
+        <h5>Notes</h5>
+        <p>Un dépot de 40% est requis pour l'avancement du projet.</p>
+    </section>
 </body>
 </html>
