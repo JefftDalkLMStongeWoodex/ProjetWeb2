0d490bc05f694e1a23d8a5ef9254de01733b13f8
Mathieu St-Onge <stng.mathieu@gmail.com>
2022-12-19 00:22:02 -0500

    Modif composant InfoCommande: ajout champs formulaire pour expedition et paiement, affichage dynamique des sous-totaux/taxes/total en fct des données province de la db et des champs sélectionnés dans le formulaire

--- LaravelApp/VDO/resources/js/Components/Checkout/InfoCommande.vue
+++ LaravelApp/VDO/resources/js/Components/Checkout/InfoCommande.vue
@@ -1,20 +1,41 @@
 <script setup>
-import { useForm } from '@inertiajs/inertia-vue3'
-import Select from '@/Components/Select.vue';
+import { useForm, Link } from '@inertiajs/inertia-vue3';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import InputLabel from '@/Components/InputLabel.vue'
+import InputError from '@/Components/InputError.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+
+
+
+
 const props = defineProps({
     taxes: Object,
     provinces: Object,
-    panier: Object
+    panier: Object,
+    modePaiement: Object,
+    modeExpedition: Object,
+    langCheckout: Object
 })
 
 const form = useForm({
-    provinces: '',
-    modeExpedition: ''
+    mode_paiements_id: '',
+    mode_expeditions_id: '',
+    panier: props.panier
 })
 
+const submit = () => {
+    form.post(route('commande.store'), { preserveState: true})
+}
 </script>
 <script>
 export default{
+    data() {
+        return {
+            livraison: false,
+            montantLivraison: 300
+        }
+    },
     computed: {
         sousTotalVoitures: function () {
             let total = 0
@@ -27,72 +48,102 @@ export default{
     methods: {
         calculTaxe (taux) {
             taux = parseFloat(taux)
-            console.log(typeof taux)
             return this.sousTotalVoitures * taux
+        },
+        radioLivraison () {
+            this.livraison = true
+            this.calculMontantTotal()
+        },
+        radioRamassage () {
+            this.livraison = false
+            this.calculMontantTotal()
+        },
+        calculMontantTotal () {
+            let total = this.sousTotalVoitures
+            for (let taxe of this.taxes) {
+                total += (this.sousTotalVoitures * taxe.taux)
+            }
+            
+            if (this.livraison == true) {
+                total += 300
+            }
+
+            return total
         }
     }
 }
 </script>
 <template>
     <aside class="infoCommande">
-        <form action="" @submit.prevent="submit">
+        <form @submit.prevent="submit">
+            <div class="erreur" v-if="form.errors.mode_paiements_id">{{ form.errors.mode_paiements_id }}</div>
+            <div class="erreur" v-if="form.errors.mode_expeditions_id">{{ form.errors.mode_expeditions_id }}</div>
+            <div class="infoCommande__reception">
+                <div>
+                    <p class="infoCommande__receptionTitre">Choisissez un mode de paiement:</p>
+                    <div v-for="paiement of modePaiement">
+                        <input 
+                            type="radio" 
+                            :id="paiement.id" 
+                            name="mode_paiements_id" 
+                            :value="paiement.id" 
+                            v-model="form.mode_paiements_id"
+                        />
+                        <label :for="paiement.id">{{paiement.nom}}</label>
+                    </div>
+                </div>
+                <div>
+                    <p class="infoCommande__receptionTitre">Choisissez un mode d'expédition:</p>
+                    <div v-for="expedition of modeExpedition">
+                        <template v-if="expedition.id == 1">
+                            <input 
+                                type="radio" 
+                                :id="expedition.id" 
+                                name="mode_expeditions_id" 
+                                :value="expedition.id" 
+                                v-model="form.mode_expeditions_id"
+                                @click="radioLivraison()"
+                            />
+                            <label v-if="expedition.id == 1" :for="expedition.id">{{expedition.nom}} (frais de {{ montantLivraison }}$)</label>
+                        </template>
+                        <template v-else-if="expedition.id == 2">
+                            <input 
+                                type="radio" 
+                                :id="expedition.id" 
+                                name="mode_expeditions_id" 
+                                :value="expedition.id" 
+                                v-model="form.mode_expeditions_id"
+                                @click="radioRamassage()"
+                            />
+                            <label :for="expedition.id">{{expedition.nom}}</label>
+                        </template>
+                    </div>
+                </div>
+            </div>
             <div>
                 <h4>Récapitulatif</h4>
-                <dd>
-                    <dt>Montant voiture(s):</dt>
-                        <dl>{{sousTotalVoitures}}</dl>
-                    <dt>Frais de livraison:</dt>
-                        <dl></dl>
-                    <dt>Sous-total:</dt>
-                        <dl></dl>
+                <dl>
+                    <dt>Voiture(s):</dt>
+                    <dd>{{sousTotalVoitures}}</dd>
+                    <template v-if="form.mode_expeditions_id == 1">
+                        <dt>Frais de livraison:</dt>
+                        <dd>{{montantLivraison}}$</dd>
+                    </template>
                     <template v-for="taxe in taxes">
                         <dt>{{taxe.nom}} ({{taxe.taux}}%)</dt>
-                            <dl>{{calculTaxe(taxe.taux)}}</dl>
+                        <dd>{{calculTaxe(taxe.taux)}}</dd>
                     </template>
                     <dt>Montant total:</dt>
-                        <dl></dl>
-                </dd>
+                    <dd>{{ calculMontantTotal(livraison) }}</dd>
+                </dl>
             </div>
-
-            <div class="infoCommande__reception">
-
-                <div class="infoCommande__receptionTitre"> Comment souhaité vous recevoir la commande? </div> 
-                <br>
-
-                <input type="radio" name="modeExpedition" v-model="modeExpedition" id="livraison" value="1">
-
-                <label for="livraison">
-                    Expédition au domicile
-                </label>
-                <br>
-                <input type="radio" name="modeExpedition" v-model="modeExpedition" id="ramassage" value="2">
-                <label for="ramassage">
-                    Ramassage aux concessionnaire
-                </label>
-
-                <div class="">
-                    <label >
-                        <input type="radio" name="reception" id="" class="radio_input" >
-                        <div class="reception__tuile"> Expédition au domicile </div>
-                    </label>
-                    <label >
-                        <input type="radio" name="reception" id="" class="radio_input">
-                        <div class="reception__tuile">Ramassage aux concessionnaire </div>
-                    </label>
-
-
-                    <div v-if="form.claimType === 'Option 1'">
-                        <span>Show me </span>
-                    </div>
-
-               </div> 
-
-
-            </div>
+            <div v-if="form.mode_paiements_id == 15">
+                    <PrimaryButton :disabled="form.processing">Payer via Paypal</PrimaryButton>
+                </div>
+                <div v-else>
+                    <PrimaryButton :disabled="form.processing">Passer la commande</PrimaryButton>
+                </div>
         </form>
-        <section>
-            {{form.modeExpedition}}
-        </section>
     </aside>
 </template>
 <style scoped>
@@ -169,8 +220,25 @@ display: none;
     
 }
 
+dl {
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+    padding-top: 1rem;
+}
 
+dl > dt,
+dl > dd {
+    width: 50%;
+}
 
+dl > dt {
+    font-weight: bold;
+}
 
+.erreur {
+    color: red;
+    font-weight: bold;
+}
 
 </style>
