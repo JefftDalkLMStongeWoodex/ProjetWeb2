29b69748e6637eae0063288594be9c785a98cee8
Jephte Damas <jephtedamas@hotmail.ca>
2022-12-19 22:38:53 -0500

    Ajout du multilingue page apropos

--- LaravelApp/VDO/lang/en/apropos.php
+++ LaravelApp/VDO/lang/en/apropos.php
@@ -1,4 +1,19 @@
 <?php
 return [
-    
+  'slogan' =>'At VDO we are here to
+  offer you a service that goes the necessary mile',
+  'best' => 'The best vehicle',
+  'vaste' => 'A WIDE CHOICE IN INVENTORY',
+  'respect' =>'At VDO, respect for our customers and our human approach to sales provide you with an exceptional shopping experience. Savor the moment!',
+  'inventaire' => 'A vast inventory',
+  'garantie' => "We transfer the manufacturer's warranty still applicable.",
+  'qualite' => 'The quality of our vehicles',
+  'shop' => "Shop inside our warehouse Easily access the interior of the vehicle and check its comfort.",
+  'experience' => "A pleasant shopping experience",
+  'bonus' => "Small bonuses to facilitate the process",
+  'online' => "At VDO, we understand that buying a vehicle is not easy and can be stressful. We make efforts to simplify your life with our long opening hours, the purchase of your old vehicle and our online reservation service.",
+  'heures' => "Open 7/7 until 9 p.m. weekdays",
+  'septjours' => "Our dealership is open 7 days a week to ensure you can shop on your schedule.",
+  'rachat' => "Redemption of your vehicle",
+  'evaluation' => "We buy back your car without obligation and at the right price, without going to the SAAQ. Assessing your vehicle takes about 30 minutes and is absolutely free.",
 ];
--- LaravelApp/VDO/lang/fr/apropos.php
+++ LaravelApp/VDO/lang/fr/apropos.php
@@ -1,4 +1,19 @@
 <?php
 return [
-    
+  'slogan' => 'Chez VDO nous sommes là pour 
+  vous offrir un service qui fait le kilomètre nécéssaire ',
+  'best' => 'Le meilleur véhicule',
+  'vaste' => 'UN VASTE CHOIX EN INVENTAIRE',
+  'respect' =>'Chez VDO, le respect de nos clients et notre approche humaine de la vente vous offre une expérience de magasinage exceptionnelle. Savourez le moment!',
+  'inventaire' => 'Un vaste inventaire',
+  'garantie' => 'Nous transférons la garantie du manufacturier encore applicable',
+  'qualite' => 'La qualité de nos véhicules',
+  'shop' => "Magasinez à l'intérieur de notre entrepôt Accédez facilement à l'intérieur du véhicule et vérifiez son confort.",
+  'experience' => "Une expérience d'achat agréable",
+  'bonus' => "Des petit bonus pour faciliter le processus",
+  'online' => "Chez VDO, nous comprenons que l'achat d'un véhicule n'est pas facile et peut causer du stress. Nous faisons des efforts pour vous simplifier la vie avec nos grandes heures d'ouverture, le rachat de votre ancien véhicule et notre service de réservation en ligne.",
+  'heures' => "Ouverte 7/7 jusqu'à 21h la semaine",
+  'septjours' => "Notre concession est ouverte 7 jours sur 7 pour vous assurer la possibilité de magasiner selon votre horaire.",
+  'rachat' => "Rachat de votre véhicule",
+  'evaluation' => "On rachète votre auto sans obligation et au juste prix, sans passer à la SAAQ. L'évaluation de votre véhicule prend environ 30 minutes et est tout à fait gratuite.",
 ];
