a6c901fe8a6d189e33b4cbe30546191da04468aa
Jephte Damas <jephtedamas@hotmail.ca>
2022-12-19 23:01:49 -0500

    ajout multiligne page contact

---
+++ LaravelApp/VDO/lang/en/contact.php
@@ -0,0 +1,17 @@
+<?php
+return [
+  'contact' =>'Contact-us',
+  'envoi' => 'SEND',
+  'droit' => 'ALL RIGHTS RESERVED',
+  'heures' => 'Opening hours',
+  'lundi'=> 'Monday',
+  'mardi'=>'Tuesday',
+  'mercredi'=>'Wednesday', 
+  'jeudi'=>'Thrusday',
+  'vendredi'=>'Friday',
+  'samedi'=>'Saturday',
+  'dimanche'=>'Sunday',
+  'message'=>'MESSAGE',
+  'courriel'=> 'EMAIL',
+  'nom'=> 'NAME',
+];
---
+++ LaravelApp/VDO/lang/fr/contact.php
@@ -0,0 +1,18 @@
+<?php
+return [
+  'contact' =>'Contactez-nous',
+  'envoi' => 'ENVOI',
+  'droit' => 'TOUS LES DROITS RÉSERVÉS',
+  'heures' => "Heures d'ouvertures",
+  'lundi'=> 'Lundi',
+  'mardi'=>'Mardi',
+  'mercredi'=>'Mercredi', 
+  'jeudi'=>'Jeudi',
+  'vendredi'=>'Vendredi',
+  'samedi'=>'Samedi',
+  'dimanche'=>'Dimanche',
+  'message'=>'MESSAGE',
+  'courriel'=> 'COURRIEL',
+  'nom'=> 'NOM',
+  
+];
